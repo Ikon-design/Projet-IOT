4659800f54631afa6113de7c48f50f91e165dacc
ikon.design <>
2021-01-20 14:58:02 +0100

    Push pour clone sur apache2

--- iot-front/source/script/script.js
+++ iot-front/source/script/script.js
@@ -1,30 +1,26 @@
 var request = new XMLHttpRequest()
 
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
-
-request.onload = function () {
-
-}
-
+request.open('GET', 'http://192.168.97.2', 'clement', 'root')
 request.send
 
-fetch('https://ghibliapi.herokuapp.com/films/')
+// request.load = function () {
+//   console.log("Load")
+// }
+
+
+fetch('http://192.168.97.2:5000/api')
   .then((response) => {
+    console.log(response)
     return response.json()
   })
   .then((data) => {
-    data.forEach((movie) => {
-        const test = document.getElementById('test')
-        console.log(movie.title)
-        test.textContent = movie.title
-        const h1 = document.createElement('h1')
-        h1.textContent = movie.title
+    data.forEach((Sondes) => {
+      console.log(Sondes)
       })
-
     console.log(data)
   })
   .catch((err) => {
-    // Do something for an error here
+    console.log(err)
   })
 
 // var data = JSON.parse(request.response)
