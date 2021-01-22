2e6517668f5bf293442ad11a03a0217a30fdc4dc
ikon.design <>
2021-01-22 09:52:23 +0100

    connection a l'api

--- iot-front/source/script/script.js
+++ iot-front/source/script/script.js
@@ -3,25 +3,45 @@ var request = new XMLHttpRequest()
 request.open('GET', 'http://192.168.97.2', 'clement', 'root')
 request.send
 
-// request.load = function () {
+request.load = function () {
 //   console.log("Load")
-// }
+}
 
 
-fetch('http://192.168.97.2:5000/api')
+// console.log(probeName)
+
+
+fetch('http://192.168.97.2:5000')
   .then((response) => {
-    console.log(response)
+    // var data = JSON.parse(request.response)
+    // console.log(data, response)
     return response.json()
   })
   .then((data) => {
-    data.forEach((Sondes) => {
-      console.log(Sondes)
-      })
-    console.log(data)
+    document.getElementById('probeName').innerHTML = (data[0][3])
+    console.log(data[0])
+    document.getElementById('temperature').innerHTML = (data[0][4] + '°')
+    document.getElementById('humidity').innerHTML = (data[0][5] + '%')
+    // title.textContent = data.currentProbe.currentName
+
+    // Dialog
+    document.getElementById('currentProbeName').value = (data[0][3])
+    document.getElementById('currentProbeLongitude').value = (data[0][1])
+    document.getElementById('currentProbeLatitude').value = (data[0][2])
   })
   .catch((err) => {
     console.log(err)
   })
 
-// var data = JSON.parse(request.response)
-// console.log(data)
+(function(){
+  var openParamsDialog = getElementById('openParamsDialog')
+  var params = getElementById('params')
+  
+  openParamsDialog.addEventListener('click', function onOpen(){
+    if (typeof params.showModal === 'function') {
+      params.showModal();
+    } else {
+      console.error("nop");
+    }
+  })
+})
